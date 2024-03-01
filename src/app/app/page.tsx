ffa4a17702a8a618572713ab3909d501f9a82360
Caio Moraes V Santos <caio.moraes@radixeng.com>
2024-02-29 21:43:30 -0300

    ✨ feat: create login workflow

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,5 +1,9 @@
-export default function Page() {
+import { auth } from "@/services/auth"
+
+export default async function Page() {
+  const session = await auth()
+
   return (
-    <h1>App Page</h1>
+    <pre>{JSON.stringify(session?.user, null, 1)}</pre>
   )
 }
--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -4,12 +4,27 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form"
+import { signIn } from "next-auth/react"
+import { toast } from "@/components/ui/use-toast"
 
 export function AuthForm() {
   const form = useForm()
 
-  const handleSubmit = form.handleSubmit((data) => {
-    console.log(data)
+  const handleSubmit = form.handleSubmit(async (data) => {
+    try {
+      await signIn('email', { email: data.email, redirect: false })
+      
+      toast({
+        title: 'Magic Link Sent',
+        description: 'Check your email for the magic link to login'
+      })
+    } catch (error) {
+      
+      toast({
+        title: 'Error',
+        description: 'An error ocurred. Please try again.'
+      })
+    }
   })
 
   return (
